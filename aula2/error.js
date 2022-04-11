1b510a1f490f611f4d0483de781994370deb8e05
git vali <abgromerovdv@gmail.com>
2022-04-11 19:12:44 -0300

    practicas de erros, diferencia entre let,var e const

---
+++ aula2/error.js
@@ -0,0 +1,7 @@
+const número = 1;
+
+console.log(número);
+
+const minhaVar= "alura";
+
+console.log(minhaVar);
---
+++ aula2/script2.js
@@ -0,0 +1,7 @@
+//tipagem dinámico
+//untyped
+let minhavar= 555;
+minhaVar= "texto";
+minhaVar= true;
+
+//Js es Multiparadigma: muitas soluçoes pra um mesmo problema;
